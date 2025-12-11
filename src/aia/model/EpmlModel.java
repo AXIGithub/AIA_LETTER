675f0b61292e184390ef2e25ce76c1cf132773d2
ratinoAG14 <ratino@astragraphia.co.id>
2025-12-11 14:39:32 +0700

    CreateModel EpmlModeel

---
+++ src/aia/model/EpmlModel.java
@@ -0,0 +1,231 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aia.model;
+
+/**
+ *
+ * @author Ratino
+ */
+public class EpmlModel {
+    private String clientNum;
+    private String owner;
+    private String alamat1;
+    private String alamat2;
+    private String alamat3;
+    private String alamat4;
+    private String alamat5;
+    private String cltpcode;
+    private String rmblPhone;
+    private String cnttype;
+    private String srcebus;
+    private String statcode;
+    private String life;
+    private String insured;
+    private String sinstamt;
+    private String billfreq;
+    private String billchnl;
+    private String longdesc;
+    private String ptdate;
+    private String benef;
+    private String batctrcde;
+    private String trdtp;
+    private String prodName;
+    private String flgSyariah;
+
+    public String getClientNum() {
+        return clientNum;
+    }
+
+    public void setClientNum(String clientNum) {
+        this.clientNum = clientNum;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAlamat1() {
+        return alamat1;
+    }
+
+    public void setAlamat1(String alamat1) {
+        this.alamat1 = alamat1;
+    }
+
+    public String getAlamat2() {
+        return alamat2;
+    }
+
+    public void setAlamat2(String alamat2) {
+        this.alamat2 = alamat2;
+    }
+
+    public String getAlamat3() {
+        return alamat3;
+    }
+
+    public void setAlamat3(String alamat3) {
+        this.alamat3 = alamat3;
+    }
+
+    public String getAlamat4() {
+        return alamat4;
+    }
+
+    public void setAlamat4(String alamat4) {
+        this.alamat4 = alamat4;
+    }
+
+    public String getAlamat5() {
+        return alamat5;
+    }
+
+    public void setAlamat5(String alamat5) {
+        this.alamat5 = alamat5;
+    }
+
+    public String getCltpcode() {
+        return cltpcode;
+    }
+
+    public void setCltpcode(String cltpcode) {
+        this.cltpcode = cltpcode;
+    }
+
+    public String getRmblPhone() {
+        return rmblPhone;
+    }
+
+    public void setRmblPhone(String rmblPhone) {
+        this.rmblPhone = rmblPhone;
+    }
+
+    public String getCnttype() {
+        return cnttype;
+    }
+
+    public void setCnttype(String cnttype) {
+        this.cnttype = cnttype;
+    }
+
+    public String getSrcebus() {
+        return srcebus;
+    }
+
+    public void setSrcebus(String srcebus) {
+        this.srcebus = srcebus;
+    }
+
+    public String getStatcode() {
+        return statcode;
+    }
+
+    public void setStatcode(String statcode) {
+        this.statcode = statcode;
+    }
+
+    public String getLife() {
+        return life;
+    }
+
+    public void setLife(String life) {
+        this.life = life;
+    }
+
+    public String getInsured() {
+        return insured;
+    }
+
+    public void setInsured(String insured) {
+        this.insured = insured;
+    }
+
+    public String getSinstamt() {
+        return sinstamt;
+    }
+
+    public void setSinstamt(String sinstamt) {
+        this.sinstamt = sinstamt;
+    }
+
+    public String getBillfreq() {
+        return billfreq;
+    }
+
+    public void setBillfreq(String billfreq) {
+        this.billfreq = billfreq;
+    }
+
+    public String getBillchnl() {
+        return billchnl;
+    }
+
+    public void setBillchnl(String billchnl) {
+        this.billchnl = billchnl;
+    }
+
+    public String getLongdesc() {
+        return longdesc;
+    }
+
+    public void setLongdesc(String longdesc) {
+        this.longdesc = longdesc;
+    }
+
+    public String getPtdate() {
+        return ptdate;
+    }
+
+    public void setPtdate(String ptdate) {
+        this.ptdate = ptdate;
+    }
+
+    public String getBenef() {
+        return benef;
+    }
+
+    public void setBenef(String benef) {
+        this.benef = benef;
+    }
+
+    public String getBatctrcde() {
+        return batctrcde;
+    }
+
+    public void setBatctrcde(String batctrcde) {
+        this.batctrcde = batctrcde;
+    }
+
+    public String getTrdtp() {
+        return trdtp;
+    }
+
+    public void setTrdtp(String trdtp) {
+        this.trdtp = trdtp;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getFlgSyariah() {
+        return flgSyariah;
+    }
+
+    public void setFlgSyariah(String flgSyariah) {
+        this.flgSyariah = flgSyariah;
+    }
+    
+    
+}
